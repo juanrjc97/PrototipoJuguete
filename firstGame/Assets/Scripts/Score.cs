da0fb40bed699fe1c3ee70f21f506cfdec2dbdff
juanj97 <juanj121297e@gmail.com>
2019-11-15 12:09:50 -0500

    Primer Commit /Incio de cambios /Juego Base

---
+++ firstGame/Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI; //permite escribir codigo para la interfaz de usuario
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+
+    public Text ScoreText;
+
+     
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        ScoreText.text = Time.timeSinceLevelLoad.ToString("0" + "s");
+
+        //Time.time.ToString("0"+ "s");  con esto se mide cuanto tiempo ha pasado desde el inicio del juego 
+    }
+}
