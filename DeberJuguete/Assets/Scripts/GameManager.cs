9400745b6082d48a76958c15159da13d459bc199
juanj97 <juanj121297e@gmail.com>
2019-11-15 19:19:34 -0500

    avance2 cambio de objetos/carretera/bloques

---
+++ DeberJuguete/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float Slowness = 5f;
+
+
+    bool gameHasEnd = false;
+
+    public GameObject gameOverUI;
+
+    public void EndGame() {
+
+        if (gameHasEnd ==false)
+        {
+            gameHasEnd = true;
+            StartCoroutine(RestartLevel());
+            gameOverUI.SetActive(true);
+
+            
+
+
+            Debug.Log("EndGame");
+            //Application.Quit();
+
+       
+        }
+        
+        
+        
+    }
+
+    IEnumerator RestartLevel() {
+        //before 1 seg
+        Time.timeScale = 1 / Slowness;
+        Time.fixedDeltaTime = Time.fixedDeltaTime / Slowness;
+
+        yield return new WaitForSeconds(5f/Slowness);
+
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * Slowness;
+
+
+        //after 1 seg
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+    }
+}
---
+++ DeberJuguete/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float ForwardForce = 2000f;
+
+    public float SidewayForce = 500f;
+
+    public float AnchoMapa = 15f;
+
+    
+
+    
+    // Update is called once per frame
+    //USE FixedUpdate() para poner las fisicas del jugador 
+    void FixedUpdate()
+    {
+        //añade una fuerza hacia adelante 
+        // rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(0, 0, ForwardForce * Time.deltaTime,ForceMode.VelocityChange); 
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+
+            rb.AddForce(SidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+
+            rb.AddForce(-SidewayForce * Time.deltaTime,0, 0, ForceMode.VelocityChange);
+        }
+        if (rb.position.y<-1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "RoadBlocker") {
+
+            ForwardForce = 0;
+            SidewayForce = 0;
+
+            FindObjectOfType<GameManager>().EndGame();
+            //Debug.Log(collisionInfo.collider.name);
+
+        }
+       
+    }
+}
